9fe7b358eaaad8ffdca8261b9c79d1e404f86f70
chromebook <a@a>
2017-02-23 16:44:48 +0900

    add battle.cc

---
+++ battle.cc
@@ -0,0 +1,14 @@
+//
+//  battle.cc
+//
+//  Created by 白川 岳史 on 2015/06/29.
+//  Copyright (c) 2015年 白川 岳史. All rights reserved.
+//
+
+#include "battle.h"
+
+//コンストラクタ
+Battle::Battle(){}
+
+//デストラクタ
+Battle::~Battle (){}
